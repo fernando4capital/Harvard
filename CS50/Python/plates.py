0a244240b574177f68e0b9300e4b194fe1ad989e
Fernando Da Silva <82740676+fernando4capital@users.noreply.github.com>
2023-11-14 12:20:55 -0500

    Harvard CS50 Python - Problems

---
+++ CS50/Python/plates.py
@@ -0,0 +1,34 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+def is_valid(s):
+    if len(s) > 6 or len(s) < 2:
+        return False
+
+    if s[0].isalpha() == False or s[1].isalpha() == False:
+        return False
+
+    if len(s) == 6:
+        if s[2].isalpha() == False or s[3].isalpha() == False:
+            return False
+
+    pointer = 0
+    while pointer < len(s):
+        if s[pointer].isalpha() == False:
+            if s[pointer] == '0':
+                return False
+            else:
+                break
+        pointer += 1
+
+    for c in s:
+        if c in [' ','?','!','.']:
+            return False
+
+    return True
+
+main()
